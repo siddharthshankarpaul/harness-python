670b7e9648e49e7d060645d59f39574404b1541f
siddharth Paul <siddharth.paul@zemosolabs.com>
2025-03-01 23:50:05 +0530

    version var

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask , render_template
+import os
 app =   Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    version = os.environ("version", 0)
+    return render_template('index.html', version_no=version)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='8090')
